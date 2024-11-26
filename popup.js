698d178fac7903c8bcd2c550a44abf923c9e9501
lilboydowny <divinityshipt@gmail.com>
2024-11-25 17:38:05 -0700

    toggle text added to slider
    slider text added to tell if it is enabled/disabled

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
-/* runs in the extension's popup window. it operates independently
-   of any specific webpage, so it can't interact directly with
-   content in webpage. might use popup.js to allow users to adjust 
-   settings for the password checker, like toggling certain security 
-   checks on or off. Then, it could send these preferences to 
-   content.js. */
-console.log("This is a popup!")
+// Select the text and slider elements
+const sliderText = document.getElementById('sliderText');
+const toggleInput = document.querySelector('.toggle');
+
+// Event listener for when the slider is toggled
+toggleInput.addEventListener('change', () => {
+    if (toggleInput.checked) {
+        sliderText.textContent = 'E᙭TEᑎᔕIOᑎ EᑎᗩᗷᒪEᗪ'; // Text for "checked" state
+        sliderText.style.color = '#95B5C7'
+    } else {
+        sliderText.textContent = 'E᙭TEᑎᔕIOᑎ ᗪIᔕᗩᗷᒪEᗪ'; // Text for "unchecked" state
+        sliderText.style.color = '#536878'
+    }
+});
